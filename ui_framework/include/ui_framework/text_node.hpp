26d60a68ee6c56d5be1362baa79d240d6cf50ff4
Valeri-afk <72664176+Valeri-afk@users.noreply.github.com>
2026-08-21 06:29:12 +0300

    Add public TextNode header

---
+++ ui_framework/include/ui_framework/text_node.hpp
@@ -0,0 +1,35 @@
+#pragma once
+
+#include "node.hpp"
+#include "ui_framework/core/text_primitive.hpp"
+
+namespace ui
+{
+    class TextNode : public Node
+    {
+    public:
+        TextNode() = default;
+        ~TextNode() override = default;
+
+        const std::string &getText() const noexcept;
+        void setText(std::string text);
+
+        TTF_Font *getFont() const noexcept;
+        void setFont(TTF_Font *font);
+
+        TextAlignment getHorizontalAlignment() const noexcept;
+        void setHorizontalAlignment(TextAlignment alignment);
+        TextAlignment getVerticalAlignment() const noexcept;
+        void setVerticalAlignment(TextAlignment alignment);
+
+        Color getColor() const noexcept;
+        void setColor(const Color &color);
+
+    protected:
+        LayoutSize measureContent(const LayoutSize &availableContent) const override;
+        void draw(SDL_Renderer *renderer) override;
+
+    private:
+        TextPrimitive text_;
+    };
+}
