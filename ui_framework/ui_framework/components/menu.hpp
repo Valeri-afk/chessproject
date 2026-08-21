8267f94237bdfa9e73304e4518ecd5bf913f7728
Valeri-afk <72664176+Valeri-afk@users.noreply.github.com>
2026-08-22 00:22:04 +0300

    Add files via upload

---
+++ ui_framework/ui_framework/components/menu.hpp
@@ -0,0 +1,35 @@
+#pragma once
+
+#include <cstddef>
+#include <functional>
+#include <memory>
+
+#include "ui_framework/components/menu_item.hpp"
+#include "ui_framework/stack_panel_node.hpp"
+
+namespace ui
+{
+    class Menu : public StackPanelNode
+    {
+    public:
+        using ItemActivationCallback = std::function<void(MenuItem &)>;
+        Menu();
+        ~Menu() override = default;
+        MenuItem *addItem(std::unique_ptr<MenuItem> item, size_t index = static_cast<size_t>(-1));
+        void removeItem(MenuItem &item);
+        void setActiveItem(MenuItem *item) noexcept;
+        MenuItem *getActiveItem() const noexcept;
+        void setSelectedItem(MenuItem *item) noexcept;
+        MenuItem *getSelectedItem() const noexcept;
+        void setItemSpacing(float spacing);
+        float getItemSpacing() const noexcept;
+        void setOnItemActivate(ItemActivationCallback callback);
+    private:
+        void syncActiveItem(MenuItem *item) noexcept;
+        void syncSelectedItem(MenuItem *item) noexcept;
+        void handleItemActivation(MenuItem &item);
+        MenuItem *activeItem_ = nullptr;
+        MenuItem *selectedItem_ = nullptr;
+        ItemActivationCallback onItemActivate_;
+    };
+}
