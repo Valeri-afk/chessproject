ebf78472ca89b0949dc8255e3b1406b9da5a257f
Valeri-afk <72664176+Valeri-afk@users.noreply.github.com>
2026-08-22 23:53:32 +0300

    Route TextNode through runtime text renderer

--- ui_framework/src/core/text_node.cpp
+++ ui_framework/src/core/text_node.cpp
@@ -1,59 +1,41 @@
 #include "ui_framework/text_node.hpp"
 
-#include "ui_framework/detail/text_primitive.hpp"
+#include "../detail/text_primitive.hpp"
 
 #include <algorithm>
 
 namespace ui
 {
-    const std::string &TextNode::getText() const noexcept { return text_.getText(); }
-
+    const std::string &TextNode::getText() const noexcept { return text_; }
     void TextNode::setText(std::string text)
     {
-        if (text_.getText() == text)
-            return;
-        deferLayoutMutation([text = std::move(text)](Node &node)
-                            { static_cast<TextNode &>(node).text_.setText(text); });
+        if (text_ == text) return;
+        deferLayoutMutation([text = std::move(text)](Node &node) { static_cast<TextNode &>(node).text_ = std::move(text); });
     }
-
-    TTF_Font *TextNode::getFont() const noexcept { return text_.getFont(); }
-
+    TTF_Font *TextNode::getFont() const noexcept { return font_; }
     void TextNode::setFont(TTF_Font *font)
     {
-        if (text_.getFont() == font)
-            return;
-        deferLayoutMutation([font](Node &node)
-                            { static_cast<TextNode &>(node).text_.setFont(font); });
+        if (font_ == font) return;
+        deferLayoutMutation([font](Node &node) { static_cast<TextNode &>(node).font_ = font; });
     }
-
-    TextAlignment TextNode::getHorizontalAlignment() const noexcept { return text_.getHorizontalAlignment(); }
-
+    TextAlignment TextNode::getHorizontalAlignment() const noexcept { return horizontalAlignment_; }
     void TextNode::setHorizontalAlignment(TextAlignment alignment)
     {
-        if (getHorizontalAlignment() == alignment)
-            return;
-        deferLayoutMutation([alignment](Node &node)
-                            { static_cast<TextNode &>(node).text_.setHorizontalAlignment(alignment); });
+        if (horizontalAlignment_ == alignment) return;
+        deferLayoutMutation([alignment](Node &node) { static_cast<TextNode &>(node).horizontalAlignment_ = alignment; });
     }
-
-    TextAlignment TextNode::getVerticalAlignment() const noexcept { return text_.getVerticalAlignment(); }
-
+    TextAlignment TextNode::getVerticalAlignment() const noexcept { return verticalAlignment_; }
     void TextNode::setVerticalAlignment(TextAlignment alignment)
     {
-        if (getVerticalAlignment() == alignment)
-            return;
-        deferLayoutMutation([alignment](Node &node)
-                            { static_cast<TextNode &>(node).text_.setVerticalAlignment(alignment); });
+        if (verticalAlignment_ == alignment) return;
+        deferLayoutMutation([alignment](Node &node) { static_cast<TextNode &>(node).verticalAlignment_ = alignment; });
     }
-
-    Color TextNode::getColor() const noexcept { return text_.getColor(); }
-    void TextNode::setColor(const Color &color) { text_.setColor(color); }
-
+    Color TextNode::getColor() const noexcept { return color_; }
+    void TextNode::setColor(const Color &color) { color_ = color; }
     LayoutSize TextNode::measureContent(const LayoutSize &availableContent) const
     {
-        return text_.measure(availableContent.width);
+        return TextPrimitive::measure(font_, text_, availableContent.width);
     }
-
     void TextNode::draw(SDL_Renderer *renderer)
     {
         const LayoutPosition position = getActualPosition();
@@ -64,6 +46,6 @@ namespace ui
         const LayoutSize contentSize{
             std::max(0.0f, size.width - border.left - border.right - padding.left - padding.right),
             std::max(0.0f, size.height - border.top - border.bottom - padding.top - padding.bottom)};
-        text_.draw(renderer, contentPosition, contentSize);
+        textPrimitive().draw(renderer, text_, font_, horizontalAlignment_, verticalAlignment_, color_, contentPosition, contentSize);
     }
 }
