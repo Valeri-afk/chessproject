82840c7cf8e5d15970c3956cc63315fcc12456f1
Valeri-afk <72664176+Valeri-afk@users.noreply.github.com>
2026-08-22 18:09:47 +0300

    Separate text layout and rasterization fonts

--- ui_framework/include/ui_framework/detail/text_primitive.hpp
+++ ui_framework/include/ui_framework/detail/text_primitive.hpp
@@ -18,6 +18,10 @@ namespace ui
         void setText(std::string text);
         TTF_Font *getFont() const noexcept;
         void setFont(TTF_Font *font) noexcept;
+        TTF_Font *getRenderFont() const noexcept;
+        void setRenderFont(TTF_Font *font) noexcept;
+        float getRasterizationScale() const noexcept;
+        void setRasterizationScale(float scale) noexcept;
         TextAlignment getHorizontalAlignment() const noexcept;
         void setHorizontalAlignment(TextAlignment alignment) noexcept;
         TextAlignment getVerticalAlignment() const noexcept;
@@ -31,6 +35,8 @@ namespace ui
         bool ensureTextObject(SDL_Renderer *renderer);
         std::string text_;
         TTF_Font *font_ = nullptr;
+        TTF_Font *renderFont_ = nullptr;
+        float rasterizationScale_ = 1.0f;
         TextAlignment horizontalAlignment_ = TextAlignment::START;
         TextAlignment verticalAlignment_ = TextAlignment::START;
         Color color_ = Colors::white;
