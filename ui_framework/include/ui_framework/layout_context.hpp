b8dfaab4ebc46704d349aefb8886b92e5e6d099e
Valeri-afk <72664176+Valeri-afk@users.noreply.github.com>
2026-08-24 04:38:51 +0300

    Add Node-based Measure and Arrange child contexts

---
+++ ui_framework/include/ui_framework/layout_context.hpp
@@ -0,0 +1,23 @@
+#pragma once
+
+#include <functional>
+
+#include "ui_framework/types.hpp"
+
+namespace ui
+{
+    class Node;
+
+    struct MeasureContext
+    {
+        LayoutSize availableContentSize{};
+        std::function<LayoutSize(Node &, const LayoutSize &)> measureChild;
+    };
+
+    struct ArrangeContext
+    {
+        LayoutPosition contentPosition{};
+        LayoutSize contentSize{};
+        std::function<void(Node &, const LayoutPosition &, const LayoutSize &)> arrangeChild;
+    };
+}
