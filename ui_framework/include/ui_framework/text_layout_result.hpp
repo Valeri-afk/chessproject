58d4406f581ddbf6bfea559b4cb253e2dbdf13a2
Valeri-afk <72664176+Valeri-afk@users.noreply.github.com>
2026-08-24 23:03:13 +0300

    Add minimal prepared text layout result

---
+++ ui_framework/include/ui_framework/text_layout_result.hpp
@@ -0,0 +1,13 @@
+#pragma once
+
+#include "ui_framework/types.hpp"
+
+namespace ui
+{
+    struct TextLayoutResult
+    {
+        LayoutSize desiredSize{};
+        float lineHeight = 0.0f;
+        int lineCount = 0;
+    };
+}
