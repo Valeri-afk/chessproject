f282f549cbeb5495544df43c71eb2e08153abb52
Valeri-afk <72664176+Valeri-afk@users.noreply.github.com>
2026-08-24 21:58:19 +0300

    Add semantic Paragraph text component

---
+++ ui_framework/include/ui_framework/components/paragraph.hpp
@@ -0,0 +1,13 @@
+#pragma once
+
+#include "ui_framework/text_node.hpp"
+
+namespace ui
+{
+    class Paragraph : public TextNode
+    {
+    public:
+        Paragraph() = default;
+        ~Paragraph() override = default;
+    };
+}
