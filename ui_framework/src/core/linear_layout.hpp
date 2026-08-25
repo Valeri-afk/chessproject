855147397bdd0cee613be40a6258ea816856a0df
Valeri-afk <72664176+Valeri-afk@users.noreply.github.com>
2026-08-25 04:51:32 +0300

    Update linear_layout.hpp

--- ui_framework/src/core/linear_layout.hpp
+++ ui_framework/src/core/linear_layout.hpp
@@ -13,7 +13,7 @@ namespace ui
         struct LinearMeasureContext
         {
             LayoutSize availableSize{};
-            std::function<LayoutSize(const Node &, const LayoutSize &)> measureChild;
+            std::function<LayoutSize(Node&, const LayoutSize&)> measureChild;
         };
 
         struct LinearArrangeContext
