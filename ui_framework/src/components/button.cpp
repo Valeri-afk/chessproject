17c43d53eada10a3575fa6ab1c3f412fd78e9bcb
Valeri-afk <72664176+Valeri-afk@users.noreply.github.com>
2026-08-25 04:26:26 +0300

    Fix TextContent include path from component source

--- ui_framework/src/components/button.cpp
+++ ui_framework/src/components/button.cpp
@@ -1,7 +1,7 @@
 #include "ui_framework/components/button.hpp"
 #include "ui_framework/primitives.hpp"
 #include "ui_framework/event_types.hpp"
-#include "text_content.hpp"
+#include "../core/text_content.hpp"
 #include <algorithm>
 #include <cmath>
 #include <utility>
