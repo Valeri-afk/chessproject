ee8fbb96f64329b021b945edf8ad4a8c3bf172b1
Valeri-afk <72664176+Valeri-afk@users.noreply.github.com>
2026-08-25 05:57:10 +0300

    Replace legacy event header include

--- ui_framework/src/components/checkbox.cpp
+++ ui_framework/src/components/checkbox.cpp
@@ -1,7 +1,7 @@
 #include "ui_framework/components/checkbox.hpp"
 
 #include "ui_framework/primitives.hpp"
-#include "ui_framework/event_types.hpp"
+#include "ui_framework/events.hpp"
 
 #include <algorithm>
 #include <utility>
