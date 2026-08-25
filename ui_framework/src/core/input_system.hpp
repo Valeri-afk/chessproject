4b5eed9ea433aaff5134762be4967cfd51fde9e7
Valeri-afk <72664176+Valeri-afk@users.noreply.github.com>
2026-08-25 22:26:21 +0300

    Revert unimplemented DragEnd guard declaration

--- ui_framework/src/core/input_system.hpp
+++ ui_framework/src/core/input_system.hpp
@@ -58,7 +58,6 @@ namespace ui
         std::optional<Node::Id> pendingFocusNodeId_;
         bool pendingClearFocus_ = false;
         bool focusTransitionInProgress_ = false;
-        bool dragEndDispatchInProgress_ = false;
 
         static void rememberNode(Node *&node, std::optional<Node::Id> &id) noexcept;
         static void clearTrackedNode(Node *&node, std::optional<Node::Id> &id) noexcept;
