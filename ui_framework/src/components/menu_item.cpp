3871244acbad45ab540d744a5a8ca90ed6cf93da
Valeri-afk <72664176+Valeri-afk@users.noreply.github.com>
2026-08-25 05:58:31 +0300

    Replace legacy event header include

--- ui_framework/src/components/menu_item.cpp
+++ ui_framework/src/components/menu_item.cpp
@@ -1,6 +1,6 @@
 #include "ui_framework/components/menu_item.hpp"
 #include "ui_framework/primitives.hpp"
-#include "ui_framework/event_types.hpp"
+#include "ui_framework/events.hpp"
 #include "../core/text_content.hpp"
 #include <algorithm>
 #include <utility>
