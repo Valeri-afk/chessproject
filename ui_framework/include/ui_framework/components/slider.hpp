f8251379cbb7344908971a334f60e51da1845504
Valeri-afk <72664176+Valeri-afk@users.noreply.github.com>
2026-08-27 01:08:55 +0300

    Replace Slider callback API with semantic event

--- ui_framework/include/ui_framework/components/slider.hpp
+++ ui_framework/include/ui_framework/components/slider.hpp
@@ -1,33 +1,29 @@
 #pragma once
 
-#include <functional>
-
 #include "ui_framework/node.hpp"
 #include "ui_framework/events.hpp"
 
 namespace ui
 {
+    struct SliderValueChangedEvent : UIEvent
+    {
+        float value = 0.0f;
+    };
+
     class Slider : public Node
     {
     public:
-        using ValueCallback = std::function<void(Slider &, float)>;
-
         Slider();
         ~Slider() override = default;
 
         void setMinimum(float minimum) noexcept;
         float getMinimum() const noexcept;
-
         void setMaximum(float maximum) noexcept;
         float getMaximum() const noexcept;
-
         void setValue(float value);
         float getValue() const noexcept;
-
         void setStep(float step) noexcept;
         float getStep() const noexcept;
-
-        void setOnValueChanged(ValueCallback callback);
 
     protected:
         LayoutSize measureContent(const LayoutSize &availableContent) const override;
@@ -38,12 +34,10 @@ namespace ui
         void handleMouseDown(MouseDownEvent &event);
         void handleMouseMove(MouseMoveEvent &event);
         void handleMouseUp(MouseUpEvent &event);
-
         float minimum_ = 0.0f;
         float maximum_ = 1.0f;
         float value_ = 0.0f;
         float step_ = 0.0f;
         bool dragging_ = false;
-        ValueCallback onValueChanged_;
     };
 }
