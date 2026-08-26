ae08b4b6f5d8025a8d95ba6ffeae14c08e2aca69
Valeri-afk <72664176+Valeri-afk@users.noreply.github.com>
2026-08-27 01:11:06 +0300

    Replace ToggleButton callback API with semantic event

--- ui_framework/include/ui_framework/components/toggle_button.hpp
+++ ui_framework/include/ui_framework/components/toggle_button.hpp
@@ -1,32 +1,28 @@
 #pragma once
 
-#include <functional>
-
 #include "ui_framework/components/button.hpp"
 
 namespace ui
 {
+    struct ToggleButtonToggledEvent : UIEvent
+    {
+        bool selected = false;
+    };
+
     class ToggleButton : public Button
     {
     public:
         ToggleButton();
         ~ToggleButton() override = default;
-
         void setSelected(bool selected) noexcept;
         bool isSelected() const noexcept;
-
-        void setOnToggle(std::function<void(ToggleButton &, bool)> callback);
-
         void toggle();
         void activate() override;
-
     protected:
         Color presentationBackgroundColor() const noexcept override;
         Color presentationBorderColor() const noexcept override;
         Color presentationTextColor() const noexcept override;
-
     private:
         bool selected_ = false;
-        std::function<void(ToggleButton &, bool)> onToggle_;
     };
 }
