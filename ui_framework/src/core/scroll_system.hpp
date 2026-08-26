92950b5eba969887c5132dbfe93bfda680b5c1be
Valeri-afk <72664176+Valeri-afk@users.noreply.github.com>
2026-08-26 23:05:20 +0300

    Keep clipping separate from ScrollState

--- ui_framework/src/core/scroll_system.hpp
+++ ui_framework/src/core/scroll_system.hpp
@@ -14,7 +14,6 @@ namespace ui
     {
         LayoutSize content{};
         ScrollOffset offset{};
-        bool clipToBoundsBeforeRegistration = false;
 
         ScrollOffset maxOffset(const LayoutSize &viewport) const noexcept;
         void clampOffset(const LayoutSize &viewport) noexcept;
