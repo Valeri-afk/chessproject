0bf6972392209529209f0f789bb54cd11eeff26a
Valeri-afk <72664176+Valeri-afk@users.noreply.github.com>
2026-08-26 09:22:38 +0300

    Add TextInput SDL routing coverage

--- ui_framework/tests/core/text_input_tests.cpp
+++ ui_framework/tests/core/text_input_tests.cpp
@@ -1,7 +1,10 @@
 #include "ui_framework/components/text_input.hpp"
 
 #include "event_dispatcher.hpp"
+#include "input_system.hpp"
 #include "node_tree.hpp"
+
+#include <SDL3/SDL.h>
 
 #include <cassert>
 #include <memory>
@@ -9,128 +12,171 @@
 
 namespace
 {
-    void testTextInputPublishesSemanticTextChanges()
+    ui::TextInput *attachTextInput(ui::NodeTree &tree)
     {
-        ui::NodeTree tree;
         auto input = std::make_unique<ui::TextInput>();
         ui::TextInput *inputPtr = input.get();
         tree.attachRoot(0, std::move(input));
+        return inputPtr;
+    }
+
+    void testTextInputPublishesSemanticTextChanges()
+    {
+        ui::NodeTree tree;
+        ui::TextInput *input = attachTextInput(tree);
 
         int changes = 0;
-        inputPtr->setOnTextChanged([&](ui::TextInput &field)
+        input->setOnTextChanged([&](ui::TextInput &field)
         {
             ++changes;
             assert(field.getText() == "hello");
         });
 
-        inputPtr->setText("hello");
-        assert(inputPtr->getText() == "hello");
+        input->setText("hello");
+        assert(input->getText() == "hello");
         assert(changes == 1);
     }
 
     void testTextInputHandlesFocusedTextEvent()
     {
         ui::NodeTree tree;
-        auto input = std::make_unique<ui::TextInput>();
-        ui::TextInput *inputPtr = input.get();
-        tree.attachRoot(0, std::move(input));
+        ui::TextInput *input = attachTextInput(tree);
 
         ui::FocusGainedEvent gained;
-        ui::EventDispatcher::dispatch(tree, inputPtr, gained, false, false);
+        ui::EventDispatcher::dispatch(tree, input, gained, false, false);
 
         ui::TextInputEvent textEvent;
         textEvent.text = "hello";
-        ui::EventDispatcher::dispatch(tree, inputPtr, textEvent, false, false);
+        ui::EventDispatcher::dispatch(tree, input, textEvent, false, false);
 
-        assert(inputPtr->getText() == "hello");
+        assert(input->getText() == "hello");
         assert(textEvent.propagationStopped);
     }
 
     void testTextInputIgnoresTextEventWithoutFocus()
     {
         ui::NodeTree tree;
-        auto input = std::make_unique<ui::TextInput>();
-        ui::TextInput *inputPtr = input.get();
-        tree.attachRoot(0, std::move(input));
+        ui::TextInput *input = attachTextInput(tree);
 
         ui::TextInputEvent textEvent;
         textEvent.text = "hello";
-        ui::EventDispatcher::dispatch(tree, inputPtr, textEvent, false, false);
+        ui::EventDispatcher::dispatch(tree, input, textEvent, false, false);
 
-        assert(inputPtr->getText().empty());
+        assert(input->getText().empty());
         assert(!textEvent.propagationStopped);
     }
 
     void testTextInputKeyboardEditingAndShiftSelection()
     {
         ui::NodeTree tree;
-        auto input = std::make_unique<ui::TextInput>();
-        ui::TextInput *inputPtr = input.get();
-        tree.attachRoot(0, std::move(input));
+        ui::TextInput *input = attachTextInput(tree);
 
         ui::FocusGainedEvent gained;
-        ui::EventDispatcher::dispatch(tree, inputPtr, gained, false, false);
+        ui::EventDispatcher::dispatch(tree, input, gained, false, false);
 
         ui::TextInputEvent textEvent;
         textEvent.text = "hello";
-        ui::EventDispatcher::dispatch(tree, inputPtr, textEvent, false, false);
+        ui::EventDispatcher::dispatch(tree, input, textEvent, false, false);
 
         ui::KeyDownEvent left;
         left.key = ui::KeyCode::LEFT;
         left.modifiers.shift = true;
-        ui::EventDispatcher::dispatch(tree, inputPtr, left, false, false);
+        ui::EventDispatcher::dispatch(tree, input, left, false, false);
 
-        assert(inputPtr->getSelectionStart() == 4);
-        assert(inputPtr->getSelectionEnd() == 5);
+        assert(input->getSelectionStart() == 4);
+        assert(input->getSelectionEnd() == 5);
 
         ui::KeyDownEvent backspace;
         backspace.key = ui::KeyCode::BACKSPACE;
-        ui::EventDispatcher::dispatch(tree, inputPtr, backspace, false, false);
+        ui::EventDispatcher::dispatch(tree, input, backspace, false, false);
 
-        assert(inputPtr->getText() == "hell");
-        assert(inputPtr->getCaretPosition() == 4);
-        assert(!inputPtr->hasSelection());
+        assert(input->getText() == "hell");
+        assert(input->getCaretPosition() == 4);
+        assert(!input->hasSelection());
     }
 
     void testTextInputCtrlASelectsAll()
     {
         ui::NodeTree tree;
-        auto input = std::make_unique<ui::TextInput>();
-        ui::TextInput *inputPtr = input.get();
-        tree.attachRoot(0, std::move(input));
+        ui::TextInput *input = attachTextInput(tree);
 
         ui::FocusGainedEvent gained;
-        ui::EventDispatcher::dispatch(tree, inputPtr, gained, false, false);
+        ui::EventDispatcher::dispatch(tree, input, gained, false, false);
 
         ui::TextInputEvent textEvent;
         textEvent.text = "hello";
-        ui::EventDispatcher::dispatch(tree, inputPtr, textEvent, false, false);
+        ui::EventDispatcher::dispatch(tree, input, textEvent, false, false);
 
         ui::KeyDownEvent ctrlA;
         ctrlA.key = ui::KeyCode::A;
         ctrlA.modifiers.ctrl = true;
-        ui::EventDispatcher::dispatch(tree, inputPtr, ctrlA, false, false);
+        ui::EventDispatcher::dispatch(tree, input, ctrlA, false, false);
 
-        assert(inputPtr->hasSelection());
-        assert(inputPtr->getSelectionStart() == 0);
-        assert(inputPtr->getSelectionEnd() == 5);
+        assert(input->hasSelection());
+        assert(input->getSelectionStart() == 0);
+        assert(input->getSelectionEnd() == 5);
     }
 
-    void testTextInputMarksLayoutInvalidationThroughTextMutationOperations()
+    void testSDLTextInputRoutesThroughInputSystemFocus()
     {
         ui::NodeTree tree;
-        auto input = std::make_unique<ui::TextInput>();
-        ui::TextInput *inputPtr = input.get();
-        tree.attachRoot(0, std::move(input));
+        ui::InputSystem inputSystem;
+        ui::TextInput *input = attachTextInput(tree);
 
-        ui::FocusGainedEvent gained;
-        ui::EventDispatcher::dispatch(tree, inputPtr, gained, false, false);
+        assert(inputSystem.focus(tree, *input));
 
-        ui::TextInputEvent textEvent;
-        textEvent.text = "hello";
-        ui::EventDispatcher::dispatch(tree, inputPtr, textEvent, false, false);
+        SDL_Event event{};
+        event.type = SDL_EVENT_TEXT_INPUT;
+        event.text.text = const_cast<char *>("hello");
 
-        assert(inputPtr->getDesiredSize() == ui::LayoutSize{} || inputPtr->getDesiredSize() != ui::LayoutSize{});
+        inputSystem.processEvent(event, tree, nullptr);
+
+        assert(input->getText() == "hello");
+    }
+
+    void testSDLTextInputDoesNotReachUnfocusedTextInput()
+    {
+        ui::NodeTree tree;
+        ui::InputSystem inputSystem;
+        ui::TextInput *first = attachTextInput(tree);
+
+        auto secondNode = std::make_unique<ui::TextInput>();
+        ui::TextInput *second = secondNode.get();
+        tree.attachRoot(1, std::move(secondNode));
+
+        assert(inputSystem.focus(tree, *first));
+
+        SDL_Event event{};
+        event.type = SDL_EVENT_TEXT_INPUT;
+        event.text.text = const_cast<char *>("hello");
+
+        inputSystem.processEvent(event, tree, nullptr);
+
+        assert(first->getText() == "hello");
+        assert(second->getText().empty());
+    }
+
+    void testSDLKeyDownRoutesModifiersToTextInput()
+    {
+        ui::NodeTree tree;
+        ui::InputSystem inputSystem;
+        ui::TextInput *input = attachTextInput(tree);
+
+        bool receivedShift = false;
+        input->on<ui::KeyDownEvent>([&](ui::KeyDownEvent &event, ui::Node &)
+        {
+            receivedShift = event.modifiers.shift;
+        });
+
+        assert(inputSystem.focus(tree, *input));
+
+        SDL_Event event{};
+        event.type = SDL_EVENT_KEY_DOWN;
+        event.key.key = SDLK_LEFT;
+
+        inputSystem.processEvent(event, tree, nullptr);
+
+        assert(!receivedShift);
     }
 }
 
@@ -141,6 +187,8 @@ int main()
     testTextInputIgnoresTextEventWithoutFocus();
     testTextInputKeyboardEditingAndShiftSelection();
     testTextInputCtrlASelectsAll();
-    testTextInputMarksLayoutInvalidationThroughTextMutationOperations();
+    testSDLTextInputRoutesThroughInputSystemFocus();
+    testSDLTextInputDoesNotReachUnfocusedTextInput();
+    testSDLKeyDownRoutesModifiersToTextInput();
     return 0;
 }
