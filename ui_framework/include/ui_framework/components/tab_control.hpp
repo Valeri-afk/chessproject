cad61cf1906b45da4a231cb2e71cde4f49bf5fb3
Valeri-afk <72664176+Valeri-afk@users.noreply.github.com>
2026-08-27 01:09:44 +0300

    Replace TabControl callback API with semantic event

--- ui_framework/include/ui_framework/components/tab_control.hpp
+++ ui_framework/include/ui_framework/components/tab_control.hpp
@@ -1,7 +1,6 @@
 #pragma once
 
 #include <cstddef>
-#include <functional>
 #include <memory>
 
 #include "ui_framework/components/tab_item.hpp"
@@ -9,14 +8,17 @@
 
 namespace ui
 {
+    struct TabSelectionChangedEvent : UIEvent
+    {
+        TabItem *selectedTab = nullptr;
+        size_t selectedIndex = static_cast<size_t>(-1);
+    };
+
     class TabControl : public StackPanelNode
     {
     public:
-        using SelectionCallback = std::function<void(TabItem &, size_t)>;
-
         TabControl();
         ~TabControl() override = default;
-
         TabItem *addTab(std::unique_ptr<TabItem> tab, size_t index = static_cast<size_t>(-1));
         void removeTab(TabItem &tab);
         bool selectTab(size_t index);
@@ -24,13 +26,10 @@ namespace ui
         void clearSelection();
         TabItem *getSelectedTab() const noexcept;
         size_t getSelectedIndex() const noexcept;
-        void setOnSelectionChanged(SelectionCallback callback);
-
     private:
         void handleTabActivation(TabItem &tab);
         TabItem *asTabItem(Node *node) const noexcept;
         TabItem *selectedTab_ = nullptr;
         size_t selectedIndex_ = static_cast<size_t>(-1);
-        SelectionCallback onSelectionChanged_;
     };
 }
